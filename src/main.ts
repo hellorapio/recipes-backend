5992c598c48d3378b0da435228fc574900d97d18
Anubis <anubis356@outlook.com>
2024-01-01 12:52:24 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { RequestMethod } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
+  await app.listen(4200);
+}
+
+bootstrap();
